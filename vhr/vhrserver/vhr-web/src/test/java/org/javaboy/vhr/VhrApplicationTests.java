455414b359b3963712c07e72457a6e42be41fc06
824559HUjunfei <junfeihu375@gmail.com>
2020-03-26 21:20:46 +0800

    测试idea提交代码---创建了一个集合

--- vhr/vhrserver/vhr-web/src/test/java/org/javaboy/vhr/VhrApplicationTests.java
+++ vhr/vhrserver/vhr-web/src/test/java/org/javaboy/vhr/VhrApplicationTests.java
@@ -1,9 +1,12 @@
 package org.javaboy.vhr;
 
+import com.mysql.cj.util.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,13 @@ public class VhrApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hjf");
+        list.add("bxk");
+        list.add("czp");
+        list.add("drl");
+        System.out.println("list = " + list);
     }
 
 }
